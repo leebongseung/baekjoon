68b2c3b888ecfd194650c091c122697364bc0b08
leebongseung <yui5227@gmail.com>
2023-10-02 15:03:33 +0900

    Merge branch 'main' of https://github.com/leebongseung/baekjoon
    # Conflicts:
    #	project/src/_22942/Main.java

--- project/src/_22942/Main.java
+++ project/src/_22942/Main.java
@@ -3,50 +3,67 @@ package _22942;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
 
-        LinkedList<LinkedList<int[]>> arr = new LinkedList<>();
-
-        for(int i=0; i< 2_000_000; i++){
-            arr.add(new LinkedList<>());
-        }
-
+        LinkedList<int[]> arr = new LinkedList<>();
 
 
         for(int i = 0; i< n; i++){
             String iStr = bf.readLine();
+            StringTokenizer st = new StringTokenizer(iStr);
 
-            int cur_mid = Integer.parseInt(iStr.split(" ")[0]);
-            int cur_range = Integer.parseInt(iStr.split(" ")[1]);
+            int cur_mid = Integer.parseInt(st.nextToken());
+            int cur_range = Integer.parseInt(st.nextToken());
 
-            int left = cur_mid - cur_range + 1_000_000;
-            int right = cur_mid + cur_range + 1_000_000;
+            int left = cur_mid - cur_range;
+            int right = cur_mid + cur_range;
 
-            if(!arr.get(left).isEmpty() || !arr.get(right).isEmpty()){
-                System.out.println("NO");
-                return;
-            }
-            for(int j = left+1; j < right; j++){
-                if(!arr.get(j).isEmpty()){
-                    for(int k =0; k < arr.get(j).size(); k++){
-                        int[] preCircle = arr.get(j).get(k); // 0: mid, 1: range
-                        int d = Math.abs(preCircle[0] - cur_mid);
-                        if(!(cur_range + preCircle[1] < d || Math.abs(cur_range - preCircle[1]) > d)){
-                            System.out.println("NO");
-                            return;
-                        }
-                    }
-                }
-            }
-            arr.get(left).addLast(new int[]{cur_mid, cur_range});
-            arr.get(right).addLast(new int[]{cur_mid, cur_range});
+            arr.addLast(new int[]{left, i});
+            arr.addLast(new int[]{right, i});
         }
 
-        System.out.println("YES");
+        // left 오름차순 정렬
+        Collections.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]){
+                    if(o1[1] > o2[1]){
+                        return o2[1] - o1[1];
+                    } else{
+                        return o1[1] - o2[1];
+                    }
+                }
+                return o2[0]- o1[0];
+            }
+        });
+
+
+        LinkedList<Integer> stack = new LinkedList<>();
+        while (!arr.isEmpty()){
+            int[] curCircle = arr.removeLast();
+            if(stack.isEmpty()) {
+                stack.push(curCircle[1]);
+                continue;
+            }
+
+            int preCircleIdx = stack.peek();
+            if(curCircle[1] == preCircleIdx){
+                stack.pop();
+            } else{
+                stack.push(curCircle[1]);
+            }
+        }
+
+        if(!stack.isEmpty()){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+        }
     }
 }
