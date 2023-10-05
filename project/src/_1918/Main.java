b6e871b0d9dacd6ea818684a4baa0791638f464e
leebongseung <yui5227@gmail.com>
2023-10-05 23:14:37 +0900

    1918 30% 실패..

--- project/src/_1918/Main.java
+++ project/src/_1918/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /*  시작 시간 : 9시 01분.
 *   죵료 시간 : 10시 20분 실패
@@ -15,48 +16,51 @@ public class Main {
         String sinput = bf.readLine();
         LinkedList<Character> stack = new LinkedList<>();
         LinkedList<Character> waitStack = new LinkedList<>();
-        int cnt = 0;
+
+
         for(int i =0; i< sinput.length(); i++){
-            if(sinput.charAt(i) == '*' || sinput.charAt(i) == '/'){
-                if(!stack.isEmpty() &&(stack.peek() == '+' || stack.peek() == '-')){
+            if(sinput.charAt(i) == '*' || sinput.charAt(i) == '/') {
+                if(stack.peekFirst() == '+' || stack.peekFirst() == '-'){
                     waitStack.push(stack.pop());
                 }
                 waitStack.push(sinput.charAt(i));
             } else if (sinput.charAt(i) == '+' || sinput.charAt(i) == '-'){
                 waitStack.push(sinput.charAt(i));
             } else if (sinput.charAt(i) == '(') {
-                waitStack.push(sinput.charAt(i));
-                cnt++;
-            } else if (sinput.charAt(i) == ')'){
-                while(cnt > 0){
-                    if(!waitStack.isEmpty() && waitStack.peek() != '('){
-                        stack.push(waitStack.pop());
-                    } else{
-                        waitStack.pop();
-                        cnt--;
-                    }
-                }
-            } else{
                 stack.push(sinput.charAt(i));
-                while(!waitStack.isEmpty()){
+                waitStack.push(sinput.charAt(i));
+            } else if (sinput.charAt(i) == ')') {
+                stack.push(sinput.charAt(i));
+                while (!waitStack.isEmpty()){
+                    if(waitStack.peek() == '('){
+                        waitStack.pop();
+                        break;
+                    }
+                    stack.push(waitStack.pop());
+                }
+            }
+            else if (sinput.charAt(i) >= 'A' && sinput.charAt(i) <= 'Z'){
+                stack.push(sinput.charAt(i));
+                while (!waitStack.isEmpty()){
                     if(waitStack.peek() == '('){
                         break;
                     }
                     stack.push(waitStack.pop());
                 }
-
             }
-//            System.out.println(stack.peek());
         }
 
-        while(!waitStack.isEmpty()){
-            if(waitStack.peek() == '('){
-                waitStack.pop();
-            }
+        while (!waitStack.isEmpty()){
             stack.push(waitStack.pop());
         }
 
+        int cnt = 0;
         while(!stack.isEmpty()){
+            cnt++;
+            if(stack.peekLast() == '(' || stack.peekLast() == ')'){
+                stack.removeLast();
+                continue;
+            }
             System.out.print(stack.removeLast());
         }
     }
