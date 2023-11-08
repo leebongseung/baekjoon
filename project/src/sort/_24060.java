38b15bb9317c6024077e2da005bd80246fb0b2ea
leebongseung <yui5227@gmail.com>
2023-11-08 14:39:58 +0900

    24060 완료

--- project/src/sort/_24060.java
+++ project/src/sort/_24060.java
@@ -1,7 +1,8 @@
 package sort;
 /*  시작 시간 : 11시 5분
-*   종료 시간 :
-* */
+ *   종료 시간 : 13시 30분
+ * */
+
 import javax.xml.transform.Result;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,54 +10,68 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _24060 {
-    public static int mergesort(int[] arr, int start, int end, int cnt, int k){
-        if(start < end){
-            int mid = (start + end) / 2;
-            mergesort(arr,start,mid, cnt,k);
-            mergesort(arr, mid+1, end, cnt,k);
-            int result = merge(arr, start, mid, end, cnt, k);
-            return result;
-        }
-        return -1;
+    static int cnt;
+
+    public static void addCnt() {
+        cnt++;
     }
 
-    public static int merge(int[] arr, int start, int mid, int end, int cnt, int k){
-        int[] tmp = new int[end+1];
-        int originstart = start;
+    public static void mergesort(int[] tmp, int[] arr, int start, int end, int k) {
+//        int result = -1;
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergesort(tmp, arr, start, mid, k);
+            mergesort(tmp, arr, mid + 1, end, k);
+            merge(tmp, arr, start, mid, end, k);
+
+        }
+    }
+
+    public static void merge(int[] tmp, int[] arr, int start, int mid, int end, int k) {
         int t = 0;
         int i = start;
-        int j = mid+1;
-        while(start <= mid && mid+1 <= end ){
-            System.out.println("start = " + start);
-            System.out.println("mid = " + mid);
-            System.out.println("end = " + end);
-            System.out.println("t = " + t);
+        int j = mid + 1;
+        while (i <= mid && j <= end) {
 
-            if(arr[i] <= arr[j]){
+            if (arr[i] <= arr[j]) {
                 tmp[t++] = arr[i++];
-            } else{
+            } else {
                 tmp[t++] = arr[j++];
-            } cnt++;
-            if(cnt == k){
-                return tmp[t];
             }
+            addCnt();
 
+            if (cnt == k) {
+                System.out.println(tmp[t - 1]);
+                System.exit(0);
+            }
         }
-        while(i <= mid){
+        while (i <= mid) {
             tmp[t++] = arr[i++];
+            addCnt();
+            if (cnt == k) {
+                System.out.println(tmp[t - 1]);
+                System.exit(0);
+            }
         }
 
-        while(j <= end){
+        while (j <= end) {
             tmp[t++] = arr[j++];
+            addCnt();
+            if (cnt == k) {
+                System.out.println(tmp[t - 1]);
+                System.exit(0);
+            }
         }
 
+        i = start;
         t = 0;
-        while(start <= end){
+        while (i <= end) {
             arr[i++] = tmp[t++];
         }
-
-        return -1;
     }
+
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
@@ -65,21 +80,15 @@ public class _24060 {
         int k = Integer.parseInt(st.nextToken()); // 저장횟수
 
         int[] arr = new int[n];
+        int[] tmp = new int[n];
 
         st = new StringTokenizer(bf.readLine());
         int idx = 0;
-        while(st.hasMoreTokens()){
+        while (st.hasMoreTokens()) {
             arr[idx] = Integer.parseInt(st.nextToken());
             idx++;
         }
-
-        int cnt =0;
-        int result = mergesort(arr, 0, n-1, cnt, k);
-        System.out.println(result);
-
-        for (int i : arr) {
-            System.out.print(i +" ");
-        }
-
+        mergesort(tmp, arr, 0, n - 1, k);
+        System.out.println(-1);
     }
 }
