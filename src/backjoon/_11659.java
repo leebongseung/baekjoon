f0b15fdef3f3b874975bf7fa3226e9c6e2cab9df
leebongseung <yui5227@gmail.com>
2024-03-21 12:38:10 +0900

    [모의고사 1일차 다시풀기]

--- src/backjoon/_11659.java
+++ src/backjoon/_11659.java
@@ -17,31 +17,24 @@ public class _11659 {
         int n = Integer.parseInt(st.nextToken()); // 전체 수의 개수
         int m = Integer.parseInt(st.nextToken()); // 덧셈 반복 횟수
 
-//        모든 입력을 받는다.
         st = new StringTokenizer(bf.readLine());
-        int[] num = new int[n];
-        int[] sum = new int[n];
-        for(int i = 0; i<n; i++){
-            num[i] = Integer.parseInt(st.nextToken());
-                //S[0] = N[0]
-            if(i == 0) sum[i] = num[i];
-                //S[i] = S[i-1] + A[i]로 S 연산하기
-            else sum[i] = sum[i-1] + num[i];
+        int sum[] = new int[n];
+        int idx = 0;
+        while(st.hasMoreTokens()){
+            if(idx == 0) sum[idx] = Integer.parseInt(st.nextToken());
+            else sum[idx] = sum[idx-1] + Integer.parseInt(st.nextToken());
+            idx++;
         }
 
         StringJoiner sj = new StringJoiner("\n");
-        for(int i =0; i<m; i++){
+        for(int i=0; i<m; i++){
             st = new StringTokenizer(bf.readLine());
             int start = Integer.parseInt(st.nextToken()) - 1;
             int end = Integer.parseInt(st.nextToken()) - 1;
 
-            // i부터 j 까지 합구하기
-            // i~j 의 합은?  S[j] - S[i-1]
-            // 만약에 i == 0 일 경우
             if(start == 0) sj.add(String.valueOf(sum[end]));
-            else sj.add(String.valueOf(sum[end] - sum[start-1]));
+            else sj.add(String.valueOf(sum[end] - sum[start - 1]));
         }
-
         System.out.println(sj);
     }
 }
