62a92414ef017752df5502fd4f135d1d39826168
leebongseung <yui5227@gmail.com>
2024-04-12 10:05:59 +0900

    문제풀이완료

--- src/backjoon/_16235.java
+++ src/backjoon/_16235.java
@@ -46,6 +46,8 @@ import java.util.PriorityQueue;
  * */
 import java.util.*;
 import java.io.*;
+
+
 public class _16235 {
     public static int n;
     public static int m; // 초기의 나무 개수
@@ -53,33 +55,6 @@ public class _16235 {
     public static int[][] s2d2;
     public static int[][] nutrientsArr; // 양분 배열
     public static List<List<PriorityQueue<Integer>>> lst = new ArrayList<>();
-
-    public static enum Direction{
-        topLeft(-1,-1),
-        top(-1,0),
-        topRight(-1,1),
-        left(0,-1),
-        right(0,1),
-        bottomLeft(1,-1),
-        bottom(1,0),
-        bottomRight(1,1);
-
-        Direction(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        private final int x;
-        private final int y;
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -90,29 +65,29 @@ public class _16235 {
 
         // 양분 배열 초기화 하기
         nutrientsArr = new int[n][n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             Arrays.fill(nutrientsArr[i], 5);
         }
 
         // s2d2 로봇 양분 세팅하기
         s2d2 = new int[n][n];
-        for(int x = 0; x<n; x++){
+        for (int x = 0; x < n; x++) {
             st = new StringTokenizer(bf.readLine());
-            for(int y =0; y< n; y++){
+            for (int y = 0; y < n; y++) {
                 s2d2[x][y] = Integer.parseInt(st.nextToken());
             }
         }
 
         // 나무 초기화
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             lst.add(new ArrayList<>());
-            for(int j=0; j<n; j++){
+            for (int j = 0; j < n; j++) {
                 lst.get(i).add(new PriorityQueue<>());
             }
         }
 
         // 나무 심기
-        for(int i=0; i<m ; i++){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(bf.readLine());
             int x = Integer.parseInt(st.nextToken()) - 1;
             int y = Integer.parseInt(st.nextToken()) - 1;
@@ -120,28 +95,52 @@ public class _16235 {
             lst.get(x).get(y).add(treeAge);
         }
 
-        for(int i=0; i<k; i++){
-            // 봄
-            for(int x=0; x<n; x++){
-                for(int y=0; y<n; y++){
-                    // 나이어린 순으로 양분 주기
-                    if(lst.get(x).get(y).isEmpty()) continue;
+//        for (List<List<Integer>> priorityQueues : lst) {
+//            for (List<Integer> priorityQueue : priorityQueues) {
+//                System.out.print("(");
+//                for (Integer integer : priorityQueue) {
+//                    System.out.print(integer + ", ");
+//                }
+//                System.out.print(")");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("===========");
 
-                    int pQSize = lst.get(x).get(y).size();
-                    // TODO : 오늘 해야할 것.
-                    for(int idx = 0; idx < pQSize; idx ++) {
-                        lst.get(x).get(y).
+        for (int i = 0; i < k; i++) {
+
+            Queue<int[]> springQ = new LinkedList<>(); // 0 : x좌표, 1:y 좌표, 2: 번식 개수.
+            // 봄
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < n; y++) {
+                    int tmpNutrientsCnt = 0; // 죽은 나무 영양분 저장하기
+                    int breedingDogs = 0; // 가을 : 번식 횟수 저장하기
+                    PriorityQueue<Integer> tmp = new PriorityQueue<>();
+                    for (int treeAge : lst.get(x).get(y)) {
                         int nutrient = nutrientsArr[x][y];
                         // 자신의 나이만큼 양분이 있는지 확인하기
                         if (treeAge > nutrient) {
                             // 여름
                             // 죽은 나무 나이 / 2 만큼 양분이 추가됨, 소수점 아래는 버림
-                            nutrientsArr[x][y] = (int) Math.floor(treeAge / 5.0);
+                            tmpNutrientsCnt += (int) Math.floor(treeAge / 2.0);
                             continue;
                         }
                         nutrientsArr[x][y] -= treeAge;
-                        lst.get(x).get(y).add((treeAge + 1));
+                        tmp.add(treeAge + 1);
+
+                        if ((treeAge + 1) % 5 == 0) breedingDogs++;
                     }
+                    lst.get(x).remove(y);
+                    lst.get(x).add(y, tmp);
+
+                    // 여름
+                    nutrientsArr[x][y] += tmpNutrientsCnt;
+
+                    // 가을 큐 짚어넣기
+                    if (breedingDogs > 0) springQ.add(new int[]{x, y, breedingDogs});
+
+                    //겨울에는 S2D2가 땅에 양분 추가함.
+                    nutrientsArr[x][y] += s2d2[x][y];
                 }
             }
 
@@ -149,43 +148,80 @@ public class _16235 {
             // 나무의 나이가 5의 배수여야함
             // 인접한 8 칸에 나이가 1인 나무가 생김
             // 상도에 벗어난 땅은 나무 안생김
-            for(int x=0; x<n; x++){
-                for(int y=0; y<n; y++) {
-                    for(int treeAge : lst.get(x).get(y)){
-                        if(treeAge % 5 == 0){
-                            for(Direction d : Direction.values()){
-                                int dx = x + d.getX();
-                                int dy = y + d.getY();
+            while (!springQ.isEmpty()) {
+                int[] poll = springQ.poll();
+                int x = poll[0];
+                int y = poll[1];
+                int breedingDogs = poll[2];
+                for (Direction d : Direction.values()) {
+                    int dx = x + d.getX();
+                    int dy = y + d.getY();
 
-                                if(dx < 0 || dx >=n || dy < 0 || dy >=n) continue;
-
-                                lst.get(dx).get(dy).add(1);
-                            }
-                            break;
-                        }
+                    if (dx < 0 || dx >= n || dy < 0 || dy >= n) continue;
+                    for (int z = 0; z < breedingDogs; z++) {
+                        lst.get(dx).get(dy).add(1);
                     }
-                    //겨울에는 S2D2가 땅에 양분 추가함.
-                    nutrientsArr[x][y] += s2d2[x][y];
                 }
-
-//                for (List<PriorityQueue<Integer>> priorityQueues : lst) {
-//                    for (PriorityQueue<Integer> priorityQueue : priorityQueues) {
-//                        System.out.print(priorityQueue.size() +" ");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println("===========");
             }
+
+//            for (List<List<Integer>> priorityQueues : lst) {
+//                for (List<Integer> priorityQueue : priorityQueues) {
+//                    System.out.print("(");
+//                    for (Integer integer : priorityQueue) {
+//                        System.out.print(integer + ", ");
+//                    }
+//                    System.out.print(")");
+//                }
+//                System.out.println();
+//            }
+//            System.out.println("-------------------------------------");
+//
+//            for (int[] ints : nutrientsArr) {
+//                for (int anInt : ints) {
+//                    System.out.print(anInt + " ");
+//                }
+//                System.out.println();
+//            }
+//            System.out.println("============================================");
         }
 
         // 살아있는 나무 개수 세기
         int res = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 res += lst.get(i).get(j).size();
             }
         }
 
         System.out.println(res);
+
+
+    }
+
+    public static enum Direction {
+        topLeft(-1, -1),
+        top(-1, 0),
+        topRight(-1, 1),
+        left(0, -1),
+        right(0, 1),
+        bottomLeft(1, -1),
+        bottom(1, 0),
+        bottomRight(1, 1);
+
+        private final int x;
+        private final int y;
+
+        Direction(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
     }
 }
