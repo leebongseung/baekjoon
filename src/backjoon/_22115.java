9ae7a3eb15163319b7c0fc2c8d4498914f9064e3
leebongseung <yui5227@gmail.com>
2024-07-09 19:44:10 +0900

    [알고리즘 풀이]

--- src/backjoon/_22115.java
+++ src/backjoon/_22115.java
@@ -3,6 +3,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -23,19 +24,30 @@ public class _22115 {
         int n = Integer.parseInt(st.nextToken()); // 커피 - 카페인 개수
         int k = Integer.parseInt(st.nextToken()); // 목표 카페인 수
 
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=1; i<=n; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[n][k+1]; // x : 커피 index, y 축: (key : 카페인 총 합, value : 개수)
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=k; k++){
-                if(i==0 || j==0)continue;
+        int[][] dp = new int[n+1][k+1]; // x : 커피 index, y 축: (key : 카페인 총 합, value : 개수)
 
-                if(j - arr[i] <= 0) continue;
-                dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j - arr[i]] + 1);
+        // x = 0 : 커피를 하나도 선택하지 않을 경우
+        // 카페인 0을 제외한 1~ k 까지는 커피를 선택할 수 없음 -> 무한대로 표기하기
+        for(int i=1; i<=k; i++){
+                dp[0][i] = 1000;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=k; j++){
+                // 현재 index의 커피를 선택할 수 있을 경우
+                if(j - arr[i] >= 0) {
+                    dp[i][j] = Math.min(dp[i - 1][j], (dp[i - 1][j - arr[i]] + 1));
+                } else{
+                    // 현재 커피 선택못하는 경우
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        System.out.println(dp[n-1][k]);
+
+        System.out.print(dp[n][k] == 0? -1 : dp[n][k]);
     }
 }
